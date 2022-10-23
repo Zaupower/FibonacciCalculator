a9847d777fcc812ed8092c0f3f2c5be457c5c8d1
Zaupower <56695737+Zaupower@users.noreply.github.com>
2022-10-23 14:06:54 +0100

    done

---
+++ FibonacciCalculatorApp/FibonacciCalculatorApp/Program.cs
@@ -0,0 +1,50 @@
+﻿using System.Numerics;
+using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int len = GetLen();
+        while (len < 0)
+        {
+            len = GetLen();
+        }
+
+        IterateFibonaci(len);
+    }
+
+    private static int GetLen()
+    {
+        string len = "";
+        int result = 0;
+        bool success = false;
+        len = Console.ReadLine();
+        success = int.TryParse(len, out result);
+
+        if (!success)
+        {
+            Console.WriteLine($"Attempted conversion of '{len ?? "<null>"}' failed.");
+        }else if (result > 250)
+        {
+            Console.WriteLine("Please insert a number bellow 250");
+        }else
+        {
+            return result;
+        }
+        return -1;
+    }
+
+    public static void IterateFibonaci(int len)
+    {
+        Console.WriteLine("LEN INSIDE" + len);
+        BigInteger a = 0, b = 1, c = 0;
+
+        for (int i = 2; i < len; i++)
+        {
+            c = a + b;
+            Console.Write(c+ " ");
+            a= b;
+            b= c;   
+        }
+    }
+}
